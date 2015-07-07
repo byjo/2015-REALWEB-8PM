6ec10dc6c0403e73fed7735c963a4491841cff6a
dragonist <erin314@naver.com>
2015-07-08 01:21:54 +0900

    [0단계] Crawler testcode

--- eight/src/test/java/pm/eight/CrawlerTest.java
+++ eight/src/test/java/pm/eight/CrawlerTest.java
@@ -1,7 +1,11 @@
 package pm.eight;
+
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -9,39 +13,44 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.Test;
 
+import pm.eight.model.Comic;
 
 public class CrawlerTest {
+	List<Comic> comics = new LinkedList();
 
 	@Test
 	public void test() throws IOException {
-		Document doc = Jsoup.connect("http://comic.naver.com/webtoon/weekday.nhn").get();
+		Document doc = Jsoup.connect(
+				"http://comic.naver.com/webtoon/weekday.nhn").get();
 		Elements elUls = doc.select(".list_area ul");
-		Elements elAll = elUls.select("li > div.thumb > a");
-//		Elements elImages = elAll.select("img[src]");
-//		Elements elImages = doc.select(".list_area ul li > div.thumb a img[src]");
-		
+		// Elements elAll = elUls.select("li > div.thumb > a");
+		// Elements elImages = elAll.select("img[src]");
+		// Elements elImages =
+		// doc.select(".list_area ul li > div.thumb a img[src]");
+
+		List<String> list = Arrays.asList("MON", "THU", "WED", "THU", "FRI",
+				"SAT", "SUN");
+
+		for (int i = 0; i < list.size(); i++) {
+			put(elUls.select("li > div.thumb > a"), list.get(i));
+		}
+
+	}
+
+	private void put(Elements elAll, String comicWeek) {
 		for (Element elOne : elAll) {
-//			id
-//			title
-//			요일
-//			에피소드 리스트 링
-//			thumnail 조그만 이미지
-			  Element elImage = elOne.select("img[src]").first();
-			  String linkHref = elImage.attr("src");
-			  String comicHref = elImage.attr("src");
-			  String comicSrc = saveImage(linkHref);
-			  String comicName = elImage.attr("title");
-			  
-			  System.out.println(elOne);
-			  System.out.println(linkHref);
-//			  String linkText = elImage.text();
-//			  System.out.println(linkHref);
+			Element elImage = elOne.select("img[src]").first();
+			String comicLink = elOne.attr("href");
+			String comicImgUrl = saveImage(elImage.attr("src"));
+			String comicTitle = elImage.attr("title");
+			Comic comic = new Comic(comicTitle, comicWeek, comicLink, comicImgUrl);
+			comics.add(comic);
+			System.out.println(comic);
 		}
 	}
 
 	private String saveImage(String linkHref) {
-		// TODO Auto-generated method stub
-		return null;
+		return linkHref;
 	}
 
 }
