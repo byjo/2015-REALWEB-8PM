9570e6127b975f79da26a88654f05707168be0a9
JB <wq1021@naver.com>
2015-08-05 22:47:35 +0900

    refactoring

--- eight/src/main/java/pm/eight/evaluator/DiligenceEvaluator.java
+++ eight/src/main/java/pm/eight/evaluator/DiligenceEvaluator.java
@@ -1,7 +1,6 @@
 package pm.eight.evaluator;
 
 import java.io.IOException;
-import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -32,12 +31,9 @@ public class DiligenceEvaluator {
 		
 		String comicPageLink = "http://comic.naver.com" + comic.getLink();
 		ComicPageDTO comicPageDTO = crawler.crawlComicPage(comicPageLink);
-		System.out.println(comicPageLink);
+		
 		String episodePageLink = comicPageDTO.getLatestLink();
 		EpisodePageDTO episodePageDTO = crawler.crawlEpisodePage(episodePageLink);
-		System.out.println(episodePageLink);
-		//Test시 필요
-//		episode.setCreateDate(new Date());
 
 		long delayTime;
 		if ((delayTime = dateManager.calculateDelayTime(episode.getCreateDate())) > 0) {
@@ -47,13 +43,8 @@ public class DiligenceEvaluator {
 			episode.setWebtookStateCode(WebtoonStateType.PUBLISH);
 		}
 
-		System.out.println("?????");
-		episode.setTitle("제1화 ㅋ");
-		System.out.println(episodePageLink);
 		episode.setLink(episodePageLink);
 		episode.setAmount(episodePageDTO.getAmount());
-		System.out.println(episode.getLink());
-		System.out.println(episode.toString());
 		
 		episodeRepository.save(episode);
 	}
